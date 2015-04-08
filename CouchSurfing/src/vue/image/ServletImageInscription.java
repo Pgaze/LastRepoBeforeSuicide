cb1e9e092988521fffa6ee4d95d532fa1ae328da
Florian <florianbenac46@gmail.com>
2015-04-08 15:31:13 +0200

    Rajout de l'image pour la page d'inscription

---
+++ CouchSurfing/src/vue/image/ServletImageInscription.java
@@ -0,0 +1,41 @@
+package vue.image;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletImageInscription
+ */
+@WebServlet("/ServletImageInscription")
+public class ServletImageInscription extends ServletImage{
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletImageInscription() {
+    	 super();
+         this.name="couchsurfing2";
+         this.type="jpg";
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		super.doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
