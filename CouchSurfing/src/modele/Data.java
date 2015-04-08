ba4000f831058422f4f34a672bcd0786c7ee828b
Maxime <janvier.maxime.mj@gmail.com>
2015-04-08 10:06:58 +0200

    Modif adresse ip

--- CouchSurfing/src/modele/Data.java
+++ CouchSurfing/src/modele/Data.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import utilitaire.ConnectionMySQL;
 
 public final class Data {
-	public static final String BDD_IP = "192.168.1.19";
+	public static final String BDD_IP = "192.168.1.3";
 	public static final String BDD_PWD = "teamBifle";
 	public static final String BDD_USER = "serveur";
 	public static final String BDD_PORT = "3306";
